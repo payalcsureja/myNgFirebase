d06a2902c62bbf82b687a9de637fbf56f456a925
payal <payalcsureja@gmail.com>
2017-05-02 16:28:09 -0400

    cmd to run stage and prod build in comments
    // ng build --environment=stage
    // ng build --prod --env=stage  (minified)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
+// ng build --prod
+
 export const environment = {
 	envName: 'prod',
   production: true
--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -1,3 +1,6 @@
+// ng build --environment=stage
+// ng build --prod --env=stage  (minified)
+
 export const environment = {
 	envName: 'stage',
   production: true

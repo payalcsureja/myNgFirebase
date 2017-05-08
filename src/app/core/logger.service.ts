7fd7ff759456fab1d2747418870bf3554a94469d
payal <payalcsureja@gmail.com>
2017-05-08 10:40:56 -0400

    logger toast custom message

--- src/app/core/logger.service.ts
+++ src/app/core/logger.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@angular/core';
 
+import { ToastService } from './toast/toast.service';
+
 @Injectable()
 export class LoggerService {
-  log(msg: string) {
+
+	constructor(private toastService: ToastService) { }
+
+	log(msg: string) {
     console.log(msg);
   }
+
+  message(msg: string) {
+    this.toastService.activate(`${msg}`);
+  }
+
   // error(msg: string) {
-  //   console.error(msg);
   // }
 }
--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -34,70 +34,8 @@ export class SkillsComponent implements OnInit {
         this.loggerService.log(error);
       },
        () => {
-        // this.loggerService.log('skill retrieval completed');
+        this.loggerService.message('Skills retrieval completed.');
       });
   }
 
 }
-
-
-// import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
-// import { Subscription } from 'rxjs/Subscription';
-
-// import { FilterTextComponent } from '../../shared/filter-text/filter-text.component';
-// import { FilterTextService } from '../../shared/filter-text/filter-text.service';
-// import { Skill } from '../shared/skill.model';
-// import { SkillService } from '../shared/skill.service';
-
-// @Component({
-//   moduleId: module.id,
-//   selector: 'story-skill-list',
-//    templateUrl: './skill-list.component.html',
-//   styleUrls: ['./skill-list.component.css']
-// })
-// export class SkillListComponent implements OnDestroy, OnInit {
-//   private dbResetSubscription: Subscription;
-
-//   skills: Skill[];
-//   filteredSkills = this.skills;
-//   @ViewChild(FilterTextComponent) filterComponent: FilterTextComponent;
-
-//   constructor(
-//     private filterService: FilterTextService,
-//     private skillService: SkillService) { }
-
-//   filterChanged(searchText: string) {
-//     this.filteredSkills = this.filterService.filter(searchText, ['id', 'name', 'type'], this.skills);
-//   }
-
-//   getSkills() {
-//     this.skills = [];
-//     this.skillService.getSkills()
-//       .subscribe(skills => {
-//         this.skills = this.filteredSkills = skills;
-//         this.filterComponent.clear();
-//       },
-//       error => {
-//         console.log('error occurred here');
-//         console.log(error);
-//       },
-//        () => {
-//         console.log('skill retrieval completed');
-//       });
-//   }
-
-//   ngOnDestroy() {
-//     this.dbResetSubscription.unsubscribe();
-//   }
-
-//   ngOnInit() {
-//     componentHandler.upgradeDom();
-//     this.getSkills();
-//     this.dbResetSubscription = this.skillService.onDbReset
-//       .subscribe(() => this.getSkills());
-//   }
-
-//   trackBySkills(index: number, skill: Skill) {
-//     return skill.id;
-//   }
-// }
